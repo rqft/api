3cadc8dbe44eb9be5e17c081c86c59403b9b4d88
Arcy <higharcswastaken@gmail.com>
2022-06-03 22:01:34 -0500

    ywea

--- dist/routes/origin.js
+++ dist/routes/origin.js
@@ -3,11 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.origin = void 0;
 const result_1 = require("../models/result");
 function origin(req, res) {
-    (0, result_1.give)(res, req.headers.origin ||
-        req.headers.referer ||
-        req.headers.host ||
-        req.socket.remoteAddress ||
-        req.headers.hostname ||
-        null);
+    (0, result_1.give)(res, req.socket.remoteAddress || null);
 }
 exports.origin = origin;
--- src/routes/origin.ts
+++ src/routes/origin.ts
@@ -1,13 +1,5 @@
 import { Request, Response } from "express";
 import { give } from "../models/result";
 export function origin(req: Request, res: Response): void {
-  give(
-    res,
-    req.headers.origin ||
-      req.headers.referer ||
-      req.headers.host ||
-      req.socket.remoteAddress ||
-      req.headers.hostname ||
-      null
-  );
+  give(res, req.socket.remoteAddress || null);
 }
